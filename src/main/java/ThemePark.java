b224ad5c457df11994b8e64fa4403e2133c15690
Martyna Stanislawska <martynastan@gmail.com>
2021-11-23 09:07:28 +0000

    arrays addad to theme park

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,21 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import jdk.nashorn.internal.parser.JSONParser;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<IReviewed> reviewed;
+
+    public ThemePark(){
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+        this.reviewed = new ArrayList<>();
+    }
+
+    
 }
+
